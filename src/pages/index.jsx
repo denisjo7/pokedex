abfbd84f75518b1b610bec1763ae9d5aa06fbf4a
Denis Jonathan <denisjonathan7@gmail.com>
2022-01-25 05:28:14 -0300

    feat: search pokemon and suggestions
    Created logic to search a pokemon and created logic to show pokemon suggestion based on search field

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from 'react';
+import {
+  createRef, useEffect, useState,
+} from 'react';
 import Image from 'next/image';
 import PokeCard from '../components/PokeCard';
 import fetchPokemons from '../services/fetchPokemons';
@@ -6,47 +8,148 @@ import nextPokemons from '../assets/images/next.svg';
 import style from '../styles/home.module.css';
 
 export default function Home() {
-  const [pokemonsList, setPokemonsList] = useState([]);
-  const [nextFetch, setNextFetch] = useState('');
-  const [prevFetch, setPrevFetch] = useState('');
+  const [currentPokeList, setCurrentPokeList] = useState([]);
+  const [nextPokeList, setNextPokeList] = useState('');
+  const [prevPokeList, setPrevPokeList] = useState('');
+  const [totalAmountPokes, setTotalAmountPokes] = useState(0);
+  const [allPokes, setAllPokes] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [pokeToSearch, setPokeToSearch] = useState('');
+  const searchInput = createRef();
 
   useEffect(() => {
     (async () => {
-      const {
-        count, next, previous, results,
-      } = await fetchPokemons();
-      console.log(count);
-      setPokemonsList(results);
-      setNextFetch(next);
-      setPrevFetch(previous);
+      const URL_ALL_POKES = `https://pokeapi.co/api/v2/pokemon?limit=${totalAmountPokes}&offset=0`;
+      const { results } = await fetchPokemons(URL_ALL_POKES);
+      const pokeNamesTemp = [];
+      results.forEach(({ name, url }) => {
+        const id = url.split('/').reverse()[1];
+        pokeNamesTemp.push({ name, id });
+      });
+      pokeNamesTemp.sort((a, b) => a.name.localeCompare(b.name));
+      setAllPokes(pokeNamesTemp);
     })();
-  }, []);
+  }, [totalAmountPokes]);
 
-  async function handlePokemons(addOrRemove) {
-    const action = (addOrRemove === 'add') ? nextFetch : prevFetch;
+  async function loadDefaultPokeList() {
     const {
       count, next, previous, results,
-    } = await fetchPokemons(action);
-    console.log(Math.ceil(count / 12));
-    setPokemonsList(results);
-    setNextFetch(next);
-    setPrevFetch(previous);
+    } = await fetchPokemons();
+    setNextPokeList(next);
+    setPrevPokeList(previous);
+    setCurrentPokeList(results);
+    setTotalAmountPokes(count);
+  }
+
+  function loadCustomPokeList() {
+    if (pokeToSearch !== '') {
+      const filteredPokeNameList = allPokes.filter(({ name }) => name.includes(pokeToSearch));
+      const customPokeList = filteredPokeNameList.map(({ id, name }) => {
+        const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+        return { name, url };
+      });
+      Promise.all(customPokeList).then((value) => {
+        setCurrentPokeList(value);
+      });
+    }
+  }
+
+  useEffect(() => {
+    loadDefaultPokeList();
+  }, []);
+
+  useEffect(() => {
+    if (!showSuggestions) setShowSuggestions(true);
+    if (pokeToSearch === '' || !allPokes.some(({ name }) => name.includes(pokeToSearch))) {
+      setShowSuggestions(false);
+      loadDefaultPokeList();
+    }
+  }, [pokeToSearch]);
+
+  async function nextOrPrevPokes(option) {
+    const action = (option === 'next') ? nextPokeList : prevPokeList;
+    const { next, previous, results } = await fetchPokemons(action);
+    setCurrentPokeList(results);
+    setNextPokeList(next);
+    setPrevPokeList(previous);
+  }
+
+  function searchHandle() {
+    setPokeToSearch(searchInput.current.value);
   }
 
   return (
-    <div className={style.home}>
+    <div
+      aria-hidden="true"
+      className={style.home}
+      onClick={({ target }) => {
+        if (target !== searchInput.current) setShowSuggestions(false);
+      }}
+    >
       <h1 className={style.title}>POKEDEX BY FREEZING</h1>
 
+      <div className={style.search_container}>
+        <div className={style.search_input_and_suggestions}>
+          <input
+            autoComplete="off"
+            className={style.search_input}
+            id="search-pokemon"
+            onChange={searchHandle}
+            onClick={() => {
+              if (!showSuggestions && pokeToSearch !== '') setShowSuggestions(true);
+            }}
+            onKeyDown={({ key }) => {
+              if (key === 'Enter' && pokeToSearch !== '') loadCustomPokeList();
+            }}
+            ref={searchInput}
+            type="text"
+            value={pokeToSearch}
+          />
+
+          {showSuggestions && (
+            <div className={style.poke_suggestions}>
+              {allPokes
+                .filter(({ name }) => name.includes(pokeToSearch) && pokeToSearch !== '')
+                .map(({ name }, index) => (
+                  <span
+                    aria-hidden="true"
+                    className={style.poke_suggestion}
+                    key={`suggestion__${name}__${index + 0}`}
+                    id={name}
+                    onClick={({ target: { id } }) => {
+                      setPokeToSearch(id);
+                      console.log(pokeToSearch);
+                      loadCustomPokeList();
+                    }}
+                  >
+                    {name}
+                  </span>
+                ))}
+            </div>
+          )}
+        </div>
+        <button
+          className={style.search_btn}
+          type="button"
+          onClick={() => {
+            if (pokeToSearch !== '') loadCustomPokeList();
+          }}
+        >
+          Search
+        </button>
+      </div>
+
       <div className={style.pokemons_container}>
-        {pokemonsList.length > 0 && pokemonsList.map(({ name, url }) => (
-          <PokeCard key={`${name}`} url={url} />
-        ))}
+        {currentPokeList.length > 0
+          && currentPokeList.map(({ name, url }, index) => (
+            <PokeCard key={`card__${name}__${index + 0}`} url={url} />
+          ))}
       </div>
 
       <div className={style.pokemons_navigation}>
         <button
           className={style.previousPokemonsBtn}
-          onClick={() => handlePokemons('remove')}
+          onClick={() => nextOrPrevPokes('previous')}
           type="button"
         >
           <Image
@@ -59,7 +162,7 @@ export default function Home() {
 
         <button
           className={style.nextPokemonsBtn}
-          onClick={() => handlePokemons('add')}
+          onClick={() => nextOrPrevPokes('next')}
           type="button"
         >
           <Image
