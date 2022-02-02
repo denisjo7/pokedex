42862470e65f80d33639b459727e142d06dacdac
Denis Jonathan <denisjonathan7@gmail.com>
2022-02-01 23:51:40 -0300

    feat: implements app provider

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,13 @@
+import AppProvider from '../context/AppProvider';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AppProvider>
+      <Component {...pageProps} />
+      ;
+    </AppProvider>
+  );
 }
 
 export default MyApp;
