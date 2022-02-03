783e53bf37197f2de0ca43fc55969aea0093de0f
Denis Jonathan <denisjonathan7@gmail.com>
2022-02-03 00:20:07 -0300

    feat: created SearchInput component to home page

---
+++ src/components/home_page/SearchInput.jsx
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react';
+import AppContext from '../../context/AppContext';
+import handleSearch from '../../helpers/handleSearch';
+import createCustomPokeList from '../../helpers/createCustomPokeList';
+import getDefaultPokeList from '../../helpers/getDefaultPokeList';
+import style from '../../styles/home.module.css';
+
+export default function SearchInput({ searchInput }) {
+  const helpersDependencies = useContext(AppContext);
+  const {
+    pokeToSearch,
+    setPokeToSearch,
+    setShowSuggestions,
+    setWasSuggested,
+    showSuggestions,
+  } = useContext(AppContext);
+
+  return (
+    <input
+      autoComplete="off"
+      className={style.search_input}
+      id="search-pokemon"
+      onChange={() => handleSearch(setPokeToSearch, searchInput)}
+      onClick={() => {
+        if (!showSuggestions && pokeToSearch !== '') setShowSuggestions(true);
+        setWasSuggested(false);
+      }}
+      onKeyDown={({ key }) => {
+        if (key === 'Enter' && pokeToSearch !== '') createCustomPokeList(helpersDependencies);
+        if (key === 'Enter' && pokeToSearch === '') getDefaultPokeList(helpersDependencies);
+      }}
+      ref={searchInput}
+      type="text"
+      value={pokeToSearch}
+    />
+  );
+}
