bf8d5c2dfccaca54aed2d0e027fb90f741d73722
Denis Jonathan <denisjonathan7@gmail.com>
2022-02-03 00:26:19 -0300

    feat: created Suggestions component to home page

---
+++ src/components/home_page/Suggestions.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import AppContext from '../../context/AppContext';
+import handleSearchOnSuggestionClick from '../../helpers/handleSearchOnSuggestionClick';
+import style from '../../styles/home.module.css';
+
+export default function Suggestions() {
+  const helpersDependencies = useContext(AppContext);
+  const { allPokes, pokeToSearch } = useContext(AppContext);
+
+  return (
+    <div className={style.poke_suggestions}>
+      {allPokes
+        .filter(({ name }) => name.includes(pokeToSearch) && pokeToSearch !== '')
+        .map(({ name }, index) => (
+          <span
+            aria-hidden="true"
+            className={style.poke_suggestion}
+            key={`suggestion__${name}__${index + 0}`}
+            id={name}
+            onClick={({ target: { id } }) => {
+              handleSearchOnSuggestionClick(helpersDependencies, id);
+            }}
+          >
+            {name}
+          </span>
+        ))}
+    </div>
+  );
+}
