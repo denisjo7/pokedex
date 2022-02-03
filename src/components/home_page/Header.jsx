7f2ca0af509ec3cdc1d3bd9044f33c86d246a771
Denis Jonathan <denisjonathan7@gmail.com>
2022-02-03 00:12:40 -0300

    feat: created Header component to home page

---
+++ src/components/home_page/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from '../../styles/home.module.css';
+
+export default function Header() {
+  return (
+    <header className={style.header}>
+      <h1 className={style.page_title}>POKEDEX BY FREEZING</h1>
+    </header>
+  );
+}
